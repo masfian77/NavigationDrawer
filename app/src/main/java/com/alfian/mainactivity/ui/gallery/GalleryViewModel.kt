e282e82e5b2eca87befc0388233e46439a176c37
ALFIAN SATRIA V.P <masfian77@gmail.com>
2019-11-03 16:08:07 +0700

    committed

---
+++ app/src/main/java/com/alfian/mainactivity/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.alfian.mainactivity.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
